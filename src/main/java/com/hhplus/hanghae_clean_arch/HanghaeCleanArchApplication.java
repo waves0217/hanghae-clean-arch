338c4eb259cce35963da27c227788adf27a426c5
waves0217 <waves0217@gmail.com>
2024-12-22 23:36:48 +0900

    Initial commit

---
+++ src/main/java/com/hhplus/hanghae_clean_arch/HanghaeCleanArchApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.hanghae_clean_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeCleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanghaeCleanArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhplus/hanghae_clean_arch/HanghaeCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.hanghae_clean_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeCleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
